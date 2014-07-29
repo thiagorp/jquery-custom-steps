16f9a1f13ab1827dbddbfc73ab6747c0166c8b9a
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2014-07-29 08:26:14 -0300

    First implementation.

--- jquery.custom.steps.js
+++ jquery.custom.steps.js
@@ -4,21 +4,39 @@
 		var settings = $.extend({}, $.fn.customSteps.defaults, options);
 
 		var buildSteps = function(element, options) {
-			var navContainer = $(options.navigationContainerSelector);
-			var stepsContainer = $(options.stepsContainerSelector);
+			var navContainer = element.find(options.navigationContainerSelector);
+			var stepsContainer = element.find(options.stepsContainerSelector);
+
+			var currentStep = 1;
 
 			return (stepsObj = {
 				restart: function() {
+					currentStep = 1;
+
+					navContainer.find(options.navigationSelector + '.active').removeClass('active');
+					stepsContainer.find(options.stepSelector + '.active').removeClass('active');
+
+					navContainer.find(options.navigationSelector + ':first-child').addClass('active');
+					stepsContainer.find(options.stepSelector + ':first-child').addClass('active');
 				},
 				numberOfSteps: function() {
+					return stepsContainer.find(options.stepSelector).length;
 				},
 				currentStep: function() {
+					return currentStep;
 				},
 				goTo: function(n) {
+					navContainer.find(options.navigationSelector + '.active').removeClass('active');
+					stepsContainer.find(options.stepSelector + '.active').removeClass('active');
+
+					navContainer.find(options.navigationSelector + ':eq(' + n + ')').addClass('active');
+					stepsContainer.find(options.stepSelector + ':eq(' + n + ')').addClass('active');
 				},
 				next: function() {
+					goTo(currentStep+1);
 				},
 				previous: function() {
+					goTo(currentStep-1);
 				}
 			});
 		};

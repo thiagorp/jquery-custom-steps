d39141b02cf859b39877405d992831b602e8fb72
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2014-07-29 08:48:54 -0300

    Add methods options.

--- jquery.custom.steps.js
+++ jquery.custom.steps.js
@@ -3,46 +3,67 @@
 	$.fn.customSteps = function(options) {
 		var settings = $.extend({}, $.fn.customSteps.defaults, options);
 
-		var buildSteps = function(element, options) {
-			var navContainer = element.find(options.navigationContainerSelector);
-			var stepsContainer = element.find(options.stepsContainerSelector);
+		var getWizard = function(el) {
+			return el.data('wizard');
+		};
 
-			var currentStep = 1;
+		var methods = {
+			init: function(options) {
+				var navContainer = this.find(options.navigationContainerSelector);
+				var stepsContainer = this.find(options.stepsContainerSelector);
 
-			return (stepsObj = {
-				restart: function() {
-					currentStep = 1;
+				var currentStep = 1;
 
-					navContainer.find(options.navigationSelector + '.active').removeClass('active');
-					stepsContainer.find(options.stepSelector + '.active').removeClass('active');
+				return (stepsObj = {
+					restart: function() {
+						currentStep = 1;
 
-					navContainer.find(options.navigationSelector + ':first-child').addClass('active');
-					stepsContainer.find(options.stepSelector + ':first-child').addClass('active');
-				},
-				numberOfSteps: function() {
-					return stepsContainer.find(options.stepSelector).length;
-				},
-				currentStep: function() {
-					return currentStep;
-				},
-				goTo: function(n) {
-					navContainer.find(options.navigationSelector + '.active').removeClass('active');
-					stepsContainer.find(options.stepSelector + '.active').removeClass('active');
+						navContainer.find(options.navigationSelector + '.active').removeClass('active');
+						stepsContainer.find(options.stepSelector + '.active').removeClass('active');
 
-					navContainer.find(options.navigationSelector + ':eq(' + n + ')').addClass('active');
-					stepsContainer.find(options.stepSelector + ':eq(' + n + ')').addClass('active');
-				},
-				next: function() {
-					goTo(currentStep+1);
-				},
-				previous: function() {
-					goTo(currentStep-1);
-				}
-			});
+						navContainer.find(options.navigationSelector + ':first-child').addClass('active');
+						stepsContainer.find(options.stepSelector + ':first-child').addClass('active');
+					},
+					numberOfSteps: function() {
+						return stepsContainer.find(options.stepSelector).length;
+					},
+					currentStep: function() {
+						return currentStep;
+					},
+					goTo: function(n) {
+						navContainer.find(options.navigationSelector + '.active').removeClass('active');
+						stepsContainer.find(options.stepSelector + '.active').removeClass('active');
+
+						navContainer.find(options.navigationSelector + ':eq(' + n + ')').addClass('active');
+						stepsContainer.find(options.stepSelector + ':eq(' + n + ')').addClass('active');
+					},
+					next: function() {
+						goTo(currentStep+1);
+					},
+					previous: function() {
+						goTo(currentStep-1);
+					}
+				});
+			},
+			next: function() {
+				var wizard = getWizard(this);
+				wizard.next();
+			},
+			previous: function() {
+				var wizard = getWizard(this);
+				wizard.previous();
+			}
 		};
 
 		return this.each(function() {
-			buildSteps(element, settings);
+			if (typeof options == 'string') {
+				if (methods[options]) {
+					methods[options].apply(this, [].slice.call(arguments, 1));
+				}
+			} else {
+				var wizard = methods.init.apply(this, arguments);
+				this.data('wizard', wizard);
+			}
 		});
 	};
 
